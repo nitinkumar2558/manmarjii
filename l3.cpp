bf496071c8abf6ac9a3bc6cda5dfb348c4d56d70
nitinkumar2558 <42238704+nitinkumar2558@users.noreply.github.com>
2018-10-13 18:14:28 +0530

    Add files via upload

---
+++ l3.cpp
@@ -0,0 +1,30 @@
+//. Write a program for kth largest, kth smallest element of an array. (Use functions : e.g. klarge (int array[], int size, int k), similarly for smallest)
+#include<iostream>
+using namespace std;
+void klargest(int aarray[], int sizeofarray);
+void ksmallest((int aarray[], int sizeofarray,int k);
+
+
+int main() {
+
+int array[10]={5,88,44,66,22,99,67,45,78,89};
+
+int x,y;
+cout<<"there is ten element in array enter 'n',get nth largest element"<<endl;
+cin>>x;
+klargest(array,10,x);
+
+cout<<"there is ten element in array enter 'n',get nth smallest element"<<endl;
+cin>>y;
+ksmallest(array,10,y);
+}
+
+
+void klargest(int aarray[], int sizeofarray,int k){
+
+
+}
+
+void ksmallest((int aarray[], int sizeofarray,int k) {
+
+}
